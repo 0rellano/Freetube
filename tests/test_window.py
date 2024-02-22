ab60a339af5f301e195ea91ebc2c4cf988ecd3e1
Ore <orellanojoaquinmarcelo@gmail.com>
2024-02-21 23:56:09 -0300

    correccion de mal test de boton

--- tests/test_window.py
+++ tests/test_window.py
@@ -18,20 +18,3 @@ def test_window_close():
     except:
         close = True
     assert close
-
-@pytest.mark.parametrize(
-        "entry_input", [
-            "Texto de Prueba",
-            "23/12/10",
-            "",
-        ]
-)
-def test_get_url(entry_input):
-    app = MainWindow()
-
-    app.entry_url.delete(0, tk.END)
-    app.entry_url.insert(0, entry_input)
-    
-    app.button_url.invoke()
-
-    assert app.get_video() == entry_input
